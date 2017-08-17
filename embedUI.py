5d8f0185be1a57a4761844f1ffe4f9c82d21be18
Antonio Piccolboni <antonio@piccolboni.info>
2017-08-16 23:00:21 -0700

    renamed links, short post UI and more

--- embedUI.py
+++ embedUI.py
@@ -1,26 +1,37 @@
-from feedbackurl import feedbackurl
 from flask import request
 
-def feedbackurl(entry, like):
-    return ("http://" + request.host + "/learn/" + ( "l" if like else "d") + "/" + entry.link)
+
+def feedbackurl(entry, well_spent):
+    return ("http://" + request.host + "/learn/" +
+            ("l" if well_spent else "d") + "/" + entry.link)
+
+
+def is_long(text):
+    return len(text) > 1000
+
 
 def embedUI_entry(entry, score):
     bar = ('<p style="BACKGROUND-COLOR: #BBD9EE">' + (
-        '<a href="{like_link}" target="_top"><font color="green">like</font></a>'
-        if not is_good else
-        '<a href="{ dislike_link }" target="_top"><font color="red">dislike</font></a>'
+        '<a href="{well_spent_link}" target="_top"><font color="green">Time Well Spent</font></a>'
+        if score < 0 else
+        '<a href="{wasted_link}" target="_top"><font color="red">Time Wasted</font></a>'
     ) + '</p>').format(
-        like_link=feedbackurl(entry=entry, like=True),
-        dislike_link=feedbackurl(entry=entry, like=False))
-    entry['description'] = bar + entry.get('description', '') + bar
+        well_spent_link=feedbackurl(entry=entry, well_spent=True),
+        wasted_link=feedbackurl(entry=entry, well_spent=False))
+    desc = entry.get('description', '')
+    entry['description'] = bar + desc + (bar if is_long(desc) else '')
     if entry.has_key('content'):
-        entry['content'][0].value = bar + entry['content'][0].value + bar
+        content = entry['content'][0].value
+        entry['content'][0].value = bar + content + (bar if len(content) > 1000\
+                                                     else '')
     if entry.has_key('title'):
-        entry['title'] = ('* ' if score >= 0 else '? ') + entry['title']
+        entry['title'] = u"{score:.2f}|{title}".format(
+            score=score, title=entry['title'])
     return entry
 
 
 def embedUI(parsed_feed, score):
     parsed_feed.entries = [
-        embedUI_helper(e, s) for e, s in zip(parsed_feed.entries, score)
+        embedUI_entry(e, s) for e, s in zip(parsed_feed.entries, score)
     ]
+    return parsed_feed
