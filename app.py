7505183dc1092c966ba1f517258c334e32384b0a
Antonio Piccolboni <antonio@piccolboni.info>
2017-08-18 08:12:42 -0700

    completed learn route, profiling

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-#ingredients: Feedcache goose infersent sklearn passive aggressive feedgen  tinydb joblib.Memory
+#ingredients:   tinydb joblib.Memory
 from flask import Flask
+import ml
 import proxy as xy
+from werkzeug.contrib.profiler import ProfilerMiddleware
 
 app = Flask(__name__)
+# app.wsgi_app = ProfilerMiddleware(app.wsgi_app)
+
 
 @app.route('/feed/<path:url>')
 def proxy(url):
     return xy.proxy(url)
 
+
 @app.route('/learn/<feedback>/<path:url>')
 def learn(feedback, url):
-    ml.learn(feedback == "l", url)
+    ml.learn_entry(feedback == "l", url)
+    return ("", 204, {})
+
 
 if __name__ == 'main':
     app.run()
