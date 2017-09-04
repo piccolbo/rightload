72321df0859f4a43a38829aa20600bcc5f6d459f
Antonio Piccolboni <antonio@piccolboni.info>
2017-09-04 10:02:18 -0700

    refactor with mini HTML DSL, show one score a % prob

--- embedUI.py
+++ embedUI.py
@@ -1,25 +1,48 @@
 from flask import request
 import math
 
-def feedbackurl(entry, well_spent):
-    return ("http://" + request.host + "/learn/" +
-            ("l" if well_spent else "d") + "/" + entry.link)
+
+def feedbackurl(link, well_spent):
+    return ("http://" + request.host + "/feedback/" +
+            ("l" if well_spent else "d") + "/" + link)
 
 
 def is_long(text):
     return len(text) > 1000
 
-def sigmoid(x):
-    return 1/(1+ math.exp(-x))
+
+# def sigmoid(x):
+#     return 1 / (1 + math.exp(-x))
+
+
+def p(style, text):
+    return '<p style="{style}">{text}</p>'.format(style=style, text=text)
+
+
+def a(href, target, text):
+    return '<a href="{href}" target="{target}">{text}</a>'.format(
+        href=href, target=target, text=text)
+
+
+def font(color, text):
+    return '<font color="{color}">{text}</font>'.format(color=color, text=text)
 
 def embedUI_entry(entry, score):
-    bar = ('<p style="BACKGROUND-COLOR: #BBD9EE">' + (
-        '<a href="{well_spent_link}" target="_top"><font color="green">Time Well Spent</font></a>'
-        if score < 0 else
-        '<a href="{wasted_link}" target="_top"><font color="red">Time Wasted</font></a>'
-    ) + '</p>').format(
-        well_spent_link=feedbackurl(entry=entry, well_spent=True),
-        wasted_link=feedbackurl(entry=entry, well_spent=False))
+    link = entry.link
+    print score
+    good_link = a(
+        href=feedbackurl(link=link, well_spent=True),
+        target="_top",
+        text=font(color="green", text="Time Well Spent"))
+    bad_link = a(
+        href=feedbackurl(link=link, well_spent=False),
+        target="_top",
+        text=font(color="red", text="Time Wasted"))
+    bar = p(
+        style="BACKGROUND-COLOR: #DBDBDB",
+        text=(good_link if score <= 0.5 else '') + \
+            (" or " if score == 0.5 else "") + \
+            (bad_link if score >= 0.5 else ''))
     desc = entry.get('description', '')
     entry['description'] = bar + desc + (bar if is_long(desc) else '')
     if entry.has_key('content'):
@@ -27,8 +50,8 @@ def embedUI_entry(entry, score):
         entry['content'][0].value = bar + content + (bar if len(content) > 1000\
                                                      else '')
     if entry.has_key('title'):
-        entry['title'] = u"{sigscore:.2f} |{score:.2f} | {title}".format(
-            sigscore=sigmoid(score), score = score, title=entry['title'])
+        entry['title'] = u"{score:} | {title}".format(
+            score=int(score * 100), title=entry['title'])
     return entry
 
 
