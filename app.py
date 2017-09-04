bb8c48afda14c46a0d7d48b3c142369c7e495ddf
Antonio Piccolboni <antonio@piccolboni.info>
2017-09-04 10:01:06 -0700

    detesters need to be defined at app level to avoid multithreading issues

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 #ingredients:   tinydb joblib.Memory
 from flask import Flask
+from datastores import training_db, classifier_db
 import ml
 import proxy as xy
 from werkzeug.contrib.profiler import ProfilerMiddleware
 
 app = Flask(__name__)
+
 # app.wsgi_app = ProfilerMiddleware(app.wsgi_app)
 
-
 @app.route('/feed/<path:url>')
-def proxy(url):
-    return xy.proxy(url)
+def feed(url):
+    return xy.proxy(url, training_db(), classifier_db())
 
 
-@app.route('/learn/<feedback>/<path:url>')
-def learn(feedback, url):
-    ml.learn_entry(feedback == "l", url)
-    return ("", 204, {})
+@app.route('/feedback/<feedback>/<path:url>')
+def feedback(feedback, url):
+    ml.store_feedback(feedback == "l", url, training_db())
+    return ("Thank you", 204, {})
+
+
+@app.route('/learn')
+def learn():
+    ml.learn(training_db(), classifier_db())
+    return ("Done", 204, {})
 
 
 if __name__ == 'main':
