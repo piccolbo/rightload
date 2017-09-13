2efdd5567be64025c1d29d651598e641954de905
Antonio Piccolboni <antonio@piccolboni.info>
2017-09-12 20:08:21 -0700

    more changes related to switch to feature_db

--- ml.py
+++ ml.py
@@ -9,9 +9,9 @@ from warnings import warn
 user = 'piccolbo@gmail.com'
 
 
-def score_entry(entry, classifier_db):
+def score_entry(entry, classifier_db, feature_db):
     try:
-        X = url2vec(entry.link, entry)
+        X = url2vec(entry.link, feature_db, entry)
         probs = classifier_db[user].predict_proba(X=X)
         return probs[:, 1].mean()
     except Exception, e:
@@ -21,8 +21,8 @@ def score_entry(entry, classifier_db):
         return 0.5
 
 
-def score_feed(parsed_feed, classifier_db):
-    return [score_entry(e, classifier_db) for e in parsed_feed.entries]
+def score_feed(parsed_feed, classifier_db, feature_db):
+    return [score_entry(e, classifier_db, feature_db) for e in parsed_feed.entries]
 
 
 def store_feedback(feedback, url, training_db):
@@ -35,17 +35,17 @@ def store_unlabelled(url, training_db):
         training_db[url] = None
 
 
-def url2vec_or_None(url):
+def url2vec_or_None(url, feature_db):
     try:
-        return url2vec(url)
+        return url2vec(url, feature_db)
     except:
         return None
 
 
-def learn(training_db, classifier_db):
     classifier = lm.LogisticRegressionCV(n_jobs=-1)
+def learn(training_db, classifier_db, feature_db):
     Xy = [(X, [int(feedback)] * X.shape[0])
-          for X, feedback in [(url2vec_or_None(url), feedback)
+          for X, feedback in [(url2vec_or_None(url, feature_db), feedback)
                               for url, feedback in training_db.iteritems()]
           if X is not None]
     X = numpy.concatenate([X_ for X_, _ in Xy], axis=0)
