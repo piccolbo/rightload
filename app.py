64101bac0bd005fe3ca594342898d9cd34dd37b2
Antonio Piccolboni <antonio@piccolboni.info>
2017-10-10 20:01:56 -0700

    return a page, but close it right away
    since from Vienna browser opens even for a 204

--- app.py
+++ app.py
@@ -29,9 +29,23 @@ def _feed(url):
 @app.route('/feedback/<feedback>/<path:url>')
 def _feedback(feedback, url):
     store_feedback(url=url, feedback=feedback == 'l', explicit=True)
-    return ("Thank you", 204, {})
     log.info("storing feedback {feedback} for {url}".format(
         feedback=feedback, url=url))
+    return ('''
+    <!DOCTYPE html
+           PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+           "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
+    <head>
+    <title>Thanks for your feedback</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    </head>
+    <body onload="self.close()">
+    <h1>Thank you for your feedback, your filter has been updated</h1>
+    </body>
+    </html>
+    ''', 200, {})
 
 
 @app.route('/learn')
