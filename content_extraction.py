899a7e290d585a89f4783461da9fbf04b16fc498
Antonio Piccolboni <antonio@piccolboni.info>
2017-11-17 22:22:53 -0800

    Added scraping of non html content

--- content_extraction.py
+++ content_extraction.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from boilerpipe.extract import Extractor
 import logging as log
+import mimeparse
 import numpy as np
 import re
 import requests
 from urlextract import URLExtract
+import textract
+import tempfile
 
 
 def _get_entry_content(entry):
@@ -32,20 +35,42 @@ class FailedExtraction(Exception):
 
 # see
 # http://tomazkovacic.com/blog/2011/06/09/evaluating-text-extraction-algorithms/
-def _scrape(url=None, html=None):
-    assert (url != html)
-    if html is not None:
-        try:
-            return Extractor(extractor='DefaultExtractor', html=html)
-        except Exception as e:
-            log.warn("Can't extract html from {html}".format(html=html[:800]))
-    else:
-        try:
-            return Extractor(extractor='DefaultExtractor', url=url)
-        except Exception as e:
-            log.warn(("Can't extract from {url} with boilerpipe " +
-                      "because of exception {e}").format(url=url, e=e))
-            return _scrape(html=requests.get(url).content)
+def _scrape_html(html):
+    try:
+        return Extractor(extractor='DefaultExtractor', html=html)
+    except Exception as e:
+        log.warn("Can't extract from" +
+                 " {html} with boilerpipe because of exception {e}".format(
+                     html=html[:800], e=e))
+        raise FailedExtraction
+
+
+def _scrape_url(url):
+    try:
+        return Extractor(extractor='DefaultExtractor', url=url)
+    except Exception as e:
+        log.warn("Can't extract from" +
+                 " {url} with boilerpipe because of exception {e}".format(
+                     url=url, e=e))
+        raise
+
+
+def _scrape2text(url):
+    response = requests.get(url)
+    doc_type = mimeparse.parse_mime_type(response.headers['Content-Type'])[1]
+    with tempfile.NamedTemporaryFile(mode="wb", suffix="." + doc_type) as fh:
+        fh.write(response.content)
+        fh.flush()
+
+        def fail():
+            raise FailedExtraction
+
+        return {
+            'html': lambda: _scrape_html(response.content).getText(),
+            'gif': fail,
+            'jpg': fail,
+            'png': fail
+        }.get(doc_type, lambda: unicode(textract.process(fh.name)))()
 
 
 def entry2url(entry):
@@ -75,7 +100,7 @@ def entry2text(entry):
 
 def _url2html(url, entry=None):
     try:
-        html = _scrape(url=url).getHTML()
+        html = _scrape_url(url).getHTML()
     except Exception as e:
         log.warn("{url} can't be scraped because of exception {e}".format(
             url=url, e=e))
@@ -87,17 +112,26 @@ def _url2html(url, entry=None):
     return html
 
 
+def _check_short(text):
+    if len(text) < 40:
+        raise FailedExtraction
+
+
 def _url2text(url, entry=None):
-    try:  # scrape url
-        text = _scrape(url=url).getText()
-        if len(text) < 40:
-            raise FailedExtraction
-    except Exception:  # scrape entry
-        try:  # scrape entry content
-            entry_content = _scrape(html=_get_entry_content(entry)).getText()
+    try:  # scrape url with boilerpipe
+        text = _scrape_url(url).getText()
+        _check_short(text)
+    except Exception:
+        try:  # scrape with textract
+            text = _scrape2text(url)
+            _check_short(text)
         except Exception:
-            entry_content = ''
-        text = (entry.title or '') + "." + (entry_content or '')
+            try:  # scrape entry content
+                entry_content = _scrape_html(
+                    _get_entry_content(entry)).getText()
+            except Exception:
+                entry_content = ''
+            text = (entry.title or '') + "." + (entry_content or '')
     if not text:
         log.error("Could not extract any text for {url}".format(url=url))
         raise FailedExtraction
