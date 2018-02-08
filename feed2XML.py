dfffe8d0b6482612a6cfd1dee48d74721d4691d6
Antonio Piccolboni <antonio@piccolboni.info>
2018-02-07 21:24:31 -0800

    documentation

--- feed2XML.py
+++ feed2XML.py
@@ -1,3 +1,4 @@
+"""Generate a feed in XML from a feedparser parsed feed."""
 from datetime import datetime
 from feedgenerator import Rss201rev2Feed, Atom1Feed
 from functools import reduce  # forward compatibility for Python 3
@@ -5,8 +6,7 @@ from time import mktime
 
 
 def feed2XML(parsed_feed):
-    'takes a parsed feed and generates an xml feed'
-
+    """Take a parsed feed and generates an xml feed."""
     def feedFactory(version):
         if version.upper().find('RSS') != -1:
             return Rss201rev2Feed
@@ -35,7 +35,7 @@ def feed2XML(parsed_feed):
 # are treated separately). Each value is a tuple with a list of paths and a
 # default value for the argument. Each path is a tuple of keys used to access
 # the nested dict-like structure created by feedparser from the root down. The
-# first path to return a falseish value is used, converted to unicode. I've
+# first path to return a trueish value is used, converted to unicode. I've
 # found a great variety of fields within the so called RSS and Atom standards.
 # This mapping is the result of trial and error and will most likely have to be
 # updated to support more feeds.
@@ -59,12 +59,49 @@ _field_map = dict(
 
 
 def _get_nested(nested_dict, path):
+    """Access nested dicts by path.
+
+    Parameters
+    ----------
+    nested_dict : dict
+        A nested dictionary (dict of nested dicts).
+    path : collection
+        A collection of keys that, applied in order, point to an element in
+        a nested dict.
+
+    Returns
+    -------
+    Any
+        An element of the nested dict.
+
+    """
     if isinstance(path, str):
         path = (path, )
     return reduce(lambda x, y: x.get(y, {}), path, nested_dict)
 
 
 def _map_entry_structure(fmap, entry):
+    """Map feed entry element to args for feed.add_item.
+
+    Parameters
+    ----------
+    fmap : a dict
+        Keys are add_item arg names, values describe how to get the value for
+        such args out of highly variable feed entries. Specifically they are
+        tuples containing different paths, aka collection of keys to use in
+        accessing a feed entry nested dict, with priority give to the first
+        that points to a trueish value; and a default value when no path points
+        to a truish value. When paths is None, we just skip that is we don't
+        know how to extract that argument from a feed entry.
+    entry : a feedparser feed entry
+
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     return {
         arg: unicode(
             reduce(lambda x, y: x or _get_nested(entry, y) or default, paths,
