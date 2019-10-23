5ba826ce1b0ba4e21fbeef56005843878ea74d7f
Antonio Piccolboni <antonio@piccolboni.info>
2019-10-23 00:17:05 -0700

    catch up with some missing docs

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,3 +1,4 @@
+"""Feature extraction turn texts into matrices."""
 from content_extraction import get_text
 from basilica import Connection
 from joblib import Memory
@@ -14,14 +15,59 @@ _memory.reduce_size()
 
 @_memory.cache(ignore=["entry"])
 def entry2mat(entry, url):
+    """Transform a feed entry into a matrix.
+
+    Only one of the two parameters needs to be supplied.
+
+    Parameters
+    ----------
+    entry : feedcache feed entry
+        The entry to transform.
+    url : type
+        The url pointing to the content of the entry .
+
+    Returns
+    -------
+    numpy matrix
+        A matrix representation of the entry content.
+
+    """
     return _text2mat(get_text(entry=entry, url=url))
 
 
 def url2mat(url):
+    """Transform a url pointing to some content into a matrix.
+
+    Parameters
+    ----------
+    url : str
+        The URL pointing to the content.
+
+    Returns
+    -------
+    Numpy Matrix
+        The matrix representation of the content identified by the URL.
+
+    """
     return entry2mat(None, url)
 
 
 def text2sentences(text, max_sentences=300):  # limit to cap latency
+    """Split a text into sentences.
+
+    Parameters
+    ----------
+    text : str
+        The text to split.
+
+    max_sentences: int
+        The maximum number of sentences to return.
+    Returns
+    -------
+    list of str
+        A list of the sentences contained in the text.
+
+    """
     return _sent_detector.tokenize(text.strip())[:max_sentences]
 
 
@@ -43,4 +89,6 @@ def _text2mat(text):
 
 
 class FailedExtraction(Exception):
+    """Exception to be raised when extraction fails."""
+
     pass
